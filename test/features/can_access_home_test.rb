da354ab41bc9bb26e8ed8b4b9895ab5896019a99
Ramin <rsedighi@gmail.com>
2014-01-16 14:43:00 -0800

    rake works

--- test/features/can_access_home_test.rb
+++ test/features/can_access_home_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 feature "CanAccessHome" do
-  scenario "the test is sound" do
+  scenario "Welcome page" do
     visit root_path
-    page.must_have_content "Welcome aboard"
+    page.must_have_content "Welcome to Ramin's static Rails BDD page."
     page.wont_have_content "Goobye All!"
   end
 end
