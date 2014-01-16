0752a3c0727b687d69ed52236580ff9cf8b36b05
Ramin <rsedighi@gmail.com>
2014-01-16 15:58:09 -0800

    Added turn gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/rails/capybara"
+require "turn/autorun"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
