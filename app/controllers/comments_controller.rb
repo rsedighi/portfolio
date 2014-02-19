f355338c07ce7c67b1b47e1d6495780ef6b33cb3
Ramin <rsedighi@gmail.com>
2014-02-18 16:21:43 -0800

    styling changes made to projects

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-
+  before_filter :load_commentable
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
