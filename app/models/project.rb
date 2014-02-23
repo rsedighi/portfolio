3aeec3fa2fc4bf5d28c8f28eb4f01de3834079d6
Ramin <rsedighi@gmail.com>
2014-02-22 21:47:47 -0800

    changed database.yml to work with pg

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ActiveRecord::Base
   validates :technologies_used, presence: true
   belongs_to :author, class_name: "User"
   has_many :comments, as: :commentable, dependent: :destroy
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :storage => s3, :bucket => ENV['AWS_BUCKET']
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
