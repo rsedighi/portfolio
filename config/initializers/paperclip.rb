69ce4380ad7f2458a7ff87834117784cf89f5d07
Ramin <rsedighi@gmail.com>
2014-02-24 14:45:30 -0800

    minor fixing in paperclip.rb

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 if Rails.env.production?
   Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-  Paperclip::Attachment.default_options[:path] = ':CFportfolio/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:path] = '/:CFportfolio/:class/:attachment/:id_partition/:style/:filename'
 end
