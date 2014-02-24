6ad74893b40f04fe4aafa9ed48a5ffe4401c5a3d
Ramin <rsedighi@gmail.com>
2014-02-24 14:24:06 -0800

    Added stuff to projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,14 @@
 class Project < ActiveRecord::Base
-  validates :technologies_used, presence: true
   belongs_to :author, class_name: "User"
   has_many :comments, as: :commentable, dependent: :destroy
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "images/:style/missing.png"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :technologies_used, presence: true
+  has_attached_file :image,
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+
 end
