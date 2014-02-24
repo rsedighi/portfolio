1b962f6639404c772a2fe2fefc9811df7a33b8fa
Ramin <rsedighi@gmail.com>
2014-02-24 14:28:26 -0800

    blah

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,8 @@
 class Project < ActiveRecord::Base
+  validates :technologies_used, presence: true
   belongs_to :author, class_name: "User"
   has_many :comments, as: :commentable, dependent: :destroy
-  validates :technologies_used, presence: true
-  has_attached_file :image,
-    :storage => :s3,
-    :s3_protocol => 'http',
-    :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
