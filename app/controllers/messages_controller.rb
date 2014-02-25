dcf79933ea1d687bbffebf4d60f1f506ccb9c10b
Ramin <rsedighi@gmail.com>
2014-02-24 17:17:04 -0800

    removed postmark gem and install postmark-rails

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,7 +24,12 @@ class MessagesController < ApplicationController
   end
 
   private
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_message
+    #   @message = Message.find(params[:id])
+    # end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
       params.require(:message).permit(:name, :email, :message)
     end
